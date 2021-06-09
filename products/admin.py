ab473bfc2e1258ecadc427f44e9986a6cf11161d
Birrellc <vaiezmedia@gmail.com>
2021-06-09 13:35:53 +0000

    Feat: setup admin for products app

--- products/admin.py
+++ products/admin.py
@@ -1,3 +1,25 @@
 from django.contrib import admin
+from .models import Art, Category
 
 # Register your models here.
+
+
+class ArtAdmin(admin.ModelAdmin):
+    list_display = (
+        'sku',
+        'name',
+        'artist',
+        'category',
+        'price',
+        'height',
+        'width',
+        'image_path',
+        'image',
+        'sold'
+    )
+
+    order = ('sku',)
+
+
+admin.site.register(Art, ArtAdmin)
+admin.site.register(Category)
