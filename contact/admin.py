77ca7a12329a7e6ef60d37f5f348ee24f4fda420
Birrellc <vaiezmedia@gmail.com>
2021-06-18 14:43:45 +0000

    Feat: setup admin table for contact app

--- contact/admin.py
+++ contact/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
+from .models import ContactDetails
 
 # Register your models here.
+
+
+class AddressesAdmin(admin.ModelAdmin):
+    list_display = (
+        'label',
+        'email',
+    )
+    ordering = ('label',)
+
+
+class NoticeAdmin(admin.ModelAdmin):
+    list_display = (
+        'notice',
+    )
+
+
+admin.site.register(ContactDetails, AddressesAdmin)
