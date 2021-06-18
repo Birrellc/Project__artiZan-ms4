516a85373c3a702fb483398445dd11814aa774c3
Birrellc <vaiezmedia@gmail.com>
2021-06-18 14:43:27 +0000

    Feat: setup contact urls and views

--- contact/views.py
+++ contact/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from .models import ContactDetails
 
 # Create your views here.
+
+
+def contact(request):
+    """ A view to show contact information """
+
+    addresses = ContactDetails.objects.all()
+
+    context = {
+        'addresses': addresses,
+    }
+
+    return render(request, 'contact/contact.html', context)
