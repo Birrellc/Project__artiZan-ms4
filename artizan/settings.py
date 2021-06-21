2993e816ccb01c7f222228ecf8e9f4040cb989b2
Birrellc <vaiezmedia@gmail.com>
2021-06-21 16:59:15 +0000

    Chore: added development variable for DEBUG

--- artizan/settings.py
+++ artizan/settings.py
@@ -26,7 +26,7 @@ load_dotenv(find_dotenv())
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['artizan-prints.herokuapp.com', 'localhost']
 
