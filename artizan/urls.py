67307dc20d38e455685a079bed67da3d797fbe1d
Birrellc <vaiezmedia@gmail.com>
2021-06-29 10:00:43 +0000

    Fix: removed url for favicon(wasnt needed)

--- artizan/urls.py
+++ artizan/urls.py
@@ -16,7 +16,6 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-from django.views.generic.base import RedirectView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -28,8 +27,4 @@ urlpatterns = [
     path('profile/', include('profiles.urls')),
     path('blog/', include('blog.urls')),
     path('contact/', include('contact.urls')),
-    path(
-        "favicon.ico",
-        RedirectView.as_view(url=staticfiles_storage.url("favicon.ico")),
-    ),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
